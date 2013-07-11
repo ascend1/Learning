47dd49b93cccc354e92a7812cd14f41f6e6b279a
Zhen Li <ricolee87@gmail.com>
2013-07-11 18:41:59 +0200

    ByteString and IOException

--- src/todo.hs
+++ src/todo.hs
@@ -1,5 +1,7 @@
 import System.Environment
+import qualified Data.ByteString.Lazy.Char8 as B
 import System.IO
+import System.IO.Error
 import System.Directory
 import Data.List
 
@@ -9,10 +11,19 @@ dispatch = [("add", add),
             ("del", del)
            ]
 
-main = do
+main = catchIOError doWork eHandler
+
+doWork = do
     (cmd:args) <- getArgs
     let (Just action) = lookup cmd dispatch
     action args
+
+eHandler :: IOError -> IO()
+eHandler e 
+    | isDoesNotExistError e = 
+        case ioeGetFileName e of Just path -> putStrLn $ path ++ " doesn't exist."
+                                 Nothing -> putStrLn "File doesn't exist."
+    | otherwise = ioError e
 
 add :: [String] -> IO()
 add [] = putStrLn "Nothing to add."
@@ -26,13 +37,13 @@ view [] = do
 del :: [String] -> IO()
 del [] = putStrLn "Nothing to delete."
 del [itemId] = do
-    content <- readFile "todo.txt"
+    content <- B.readFile "todo.txt"
     (tempName, tempHandle) <- openTempFile "." "temp"
     let
         index = read itemId
-        items = lines content
+        items = B.lines content
         newItems = delete (items !! index) items
-    hPutStr tempHandle $ unlines newItems
+    B.hPutStr tempHandle $ B.unlines newItems
     hClose tempHandle
     removeFile "todo.txt"
     renameFile tempName "todo.txt"
