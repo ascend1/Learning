4a220c91e55a28e961869735c74dda4cd3b6a65f
Zhen Li <ricolee87@gmail.com>
2013-07-29 11:49:51 +0200

    avl tree
    implementation is bad. every time the tree has to be extracted from a
    pair: snd (bal, tree)

--- src/avltree.hs
+++ src/avltree.hs
@@ -1,11 +1,8 @@
 data Tree a = EmptyTree | Node a Bal (Tree a) (Tree a) deriving (Show, Read, Eq)
-data Bal = Same | Less | More deriving (Show, Ord, Eq)
-
-create :: a -> Tree a
-create a = Node a Same EmptyTree EmptyTree
+data Bal = Same | Less | More deriving (Show, Read, Ord, Eq)
 
 insert :: (Ord a) => a -> Tree a -> (Bal, Tree a)
-insert a EmptyTree = same $ create a
+insert a EmptyTree = more $ Node a Same EmptyTree EmptyTree
 insert a (Node x b left right)
     | a == x = same $ Node x b left right
     | a < x = case insert a left of
@@ -24,17 +21,17 @@ insert a (Node x b left right)
 rotR :: a -> Tree a -> Tree a -> (Bal, Tree a)
 rotR v (Node lv Same ll lr) r = more $ Node lv Less ll (Node v More lr r)
 rotR v (Node lv More ll lr) r = same $ Node lv Same ll (Node v Same lr r)
-rotR v (Node lv Less ll (Node lrv Same lrl lrr)) r = 
-rotR v (Node lv Less ll (Node lrv Less lrl lrr)) r =
-rotR v (Node lv Less ll (Node lrv More lrl lrr)) r =
+rotR v (Node lv Less ll (Node lrv Same lrl lrr)) r = same $ Node lrv Same (Node lv Same ll lrl) (Node v Same lrr r)
+rotR v (Node lv Less ll (Node lrv Less lrl lrr)) r = same $ Node lrv Same (Node lv More ll lrl) (Node v Same lrr r)
+rotR v (Node lv Less ll (Node lrv More lrl lrr)) r = same $ Node lrv Same (Node lv Same ll lrl) (Node v Less lrr r)
 
 
-rotL :: a -> Treea -> Tree a -> (Bal, Tree a)
+rotL :: a -> Tree a -> Tree a -> (Bal, Tree a)
 rotL v l (Node rv Same rl rr) = more $ Node rv More (Node v Less l rl) rr
 rotL v l (Node rv Less rl rr) = same $ Node rv Same (Node v Same l rl) rr
-rotL v l (Node rv More (Node rlv Same rll rlr) rr) =
-rotL v l (Node rv More (Node rlv Less rll rlr) rr) =
-rotL v l (Node rv More (Node rlv More rll rlr) rr) =
+rotL v l (Node rv More (Node rlv Same rll rlr) rr) = same $ Node rv Same (Node v Same l rll) (Node rlv Same rlr rr)
+rotL v l (Node rv More (Node rlv Less rll rlr) rr) = same $ Node rv Same (Node v More l rll) (Node rlv Same rlr rr)
+rotL v l (Node rv More (Node rlv More rll rlr) rr) = same $ Node rv Same (Node v Same l rll) (Node rlv Less rlr rr)
 
 
 find :: (Ord a) => a -> Tree a -> Bool
